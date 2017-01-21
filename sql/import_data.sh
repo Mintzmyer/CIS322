30b98d7c27c86a35e07d9a486428d969bdca69ed
Mintzmyer <samimintzmyer@gmail.com>
2017-01-21 22:01:42 +0000

    import_data curls Legacy and extracts from tar. Then cleans up

---
+++ sql/import_data.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+mkdir $HOME/LegacyTar
+cd $HOME/LegacyTar
+
+curl https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz > osnap_legacy.tar.gz
+cd $HOME
+tar xvf LegacyTar/osnap_legacy.tar.gz
+
+rm -r LegacyTar
