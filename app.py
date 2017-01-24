b781b3771115d0b7f85e46984ed952f5f0190f78
Mintzmyer <samimintzmyer@gmail.com>
2017-01-24 16:16:06 +0000

    Updated app.py for reportfilter

--- app.py
+++ app.py
@@ -10,6 +10,25 @@ def index():
 def welcome():
     return render_template('welcome.html')
 
+@app.route('/login')
+def login():
+    if request.method=='GET' and 'username' and 'login' in request.args:
+        return render_template('welcome.html', user=request.args.get('username'), password=request.args.get('password'))
+
+    if request.method=='POST' and 'username' and 'login' in request.form:
+        return render_template('welcome.html', user=request.form['username'])
+    return render_template('login.html')
+
+@app.route('/reportfilter')
+def reportfilter():
+    if request.method=='GET' and 'report' and 'assetIn' in request.form:
+        return render_template('index.html', report=request.args.get('report'), assetsIn=request.args.get('assetIn'))
+
+    if request.method=='POST' and 'report' and 'assetIn' in request.form:
+        return render_template('index.html', report=request.form['report'], assetsIn=request.form['assetIn'])
+    return render_template('reportfilter.html')
+
+
 #@app.route('/goodbye')
 #def goodbye();
 #    if request.method=='GET' and 'mytext' in request.args:
