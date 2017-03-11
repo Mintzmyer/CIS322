97bd0a6c72413e148b8b299f207f1111d9087370
Mintzmyer <samimintzmyer@gmail.com>
2017-03-11 21:55:55 +0000

    Added usersDump file to convert DB to .csv file

--- export/export_data.sh
+++ export/export_data.sh
@@ -8,8 +8,13 @@ fi
 # <dbname> Running on localhost 127.0.0.1 at port 5432
 # <output dir> Path where data files should be read from. If DNE, create dir. If Exists, rm content
 
-# users.csv - a csv file containing user information
-# facilities.csv - a csv file containing LOST facility information
-# assets.csv - a csv file containing LOST assets
-# transfers.csv - a csv file containing transfer information
 
+
+# users.csv - a csv file containing user information
+python usersDump.py
+# facilities.csv - a csv file containing LOST facility information
+python facilitiesDump.py
+# assets.csv - a csv file containing LOST assets
+python assetsDump.py
+# transfers.csv - a csv file containing transfer information
+python transfersDump.py
