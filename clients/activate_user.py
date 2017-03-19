b1181b29da9022d9aa65d6d0c30dca1266b15380
Mintzmyer <samimintzmyer@gmail.com>
2017-03-19 21:21:46 +0000

    Skeleton template for CLI client

---
+++ clients/activate_user.py
@@ -0,0 +1,5 @@
+if not (len(sys.argv)==5):
+    print("usage: activate_user.py <host name> <new username> <password> <role:facofc or logofc>")
+    quit()
+
+
---
+++ clients/revoke_user.py
@@ -0,0 +1,5 @@
+if not (len(sys.argv)==3):
+    print("usage: revoke_user.py <host name> <existing username>")
+    quit()
+
+
